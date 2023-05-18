50699ddd2bef7078aa948d54b23ad419f12f73e4
valerie_l <contacttovalerie@gmail.com>
2023-05-18 18:06:18 -0400

    Fixed confetti width

--- src/App.js
+++ src/App.js
@@ -4,7 +4,11 @@ import './App.css';
 function App() {
   return (
     <div className="App"> 
-      <Confetti friction={0.95}/>
+      <Confetti    
+      width={window.innerWidth}
+      height={window.innerHeight}
+      friction={0.95}/>
+      
       <h1>Cool! I'm working with React on my computer!</h1>
       <p>Is this what happiness feels like?</p>
     </div>
